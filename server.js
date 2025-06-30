2da2da32659112dea1f8b7971c39870550de9504
suka <khiemnguyen.hye@gmail.com>
2025-06-30 09:56:30 +0700

    Changes

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const WebSocket = require('ws');
+const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8080 })
 
 wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('message', (message) => {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
-      }
-    });
-  });
-  ws.on('close', () => {
-    console.log('Client disconnected');
-  });
-});
+    console.log('Client connected')
+    ws.on('message', (message) => {
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message.toString())
+            }
+        })
+    })
+    ws.on('close', () => {
+        console.log('Client disconnected')
+    })
+})
 
-console.log('WebSocket server running on ws://localhost:8080');
+console.log('WebSocket server running on ws://localhost:8080')
